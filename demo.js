625cb978dd87c901a3fc0f954753018b5f90b5bc
kranthi-v <krkranthikumar2003@gmail.com>
2025-10-03 13:58:40 +0530

    first javascript program

---
+++ demo.js
@@ -0,0 +1,16 @@
+console.log("Hello from gobal");
+
+function main() 
+{
+    console.log("Hello from function");
+}
+main()
+
+class demo 
+{
+    static main() 
+    {
+        console.log("Hello from class");
+    }
+}
+demo.main()
